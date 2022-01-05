f6b7da488d325ac6c68637bab490b1c0db4c1d5a
TheisFerre <theisferre@gmail.com>
2022-01-05 13:27:57 +0100

    updated some stuff

--- assignment1/matmult.c
+++ assignment1/matmult.c
@@ -20,16 +20,22 @@ void matmult_nat(int M, int N, int K, double **A, double **B, double **C) {
 
 void matmult_lib(int M, int N, int K, double **A, double **B, double **C){
 
+    // Pointers to cblass
     double * a_point = A[0];
     double * b_point = B[0];
     double * c_point = C[0];
 
+    // first dimension of A
     int lda = M;
+    // first dimension of B
     int ldb = K;
+    // first dimension of C
     int ldc = M;
 
+    // call cblas library
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0, a_point, lda, b_point, ldb, 0.0, c_point, ldc);
 
+    // free memory
     free(a_point);
     free(b_point);
     free(c_point);
