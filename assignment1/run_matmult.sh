8c301f5801c7a5a34562babb59ba738858a90dbd
Andreas Aspe <s174197@student.dtu.dk>
2022-01-05 14:11:32 +0100

    New run file

--- assignment1/run_matmult.sh
+++ assignment1/run_matmult.sh
@@ -2,11 +2,11 @@
 
 CC=${1-"gcc"}
 
-NPARTS = "10 1000 2000 3000 4000 5000 7500 10000 20000 40000 80000 200000 400000 800000 1200000 1600000 3000000"
+NPARTS = "10 200 500 1000 2000 3000 4000 5000 7500 10000 20000"
 PERM="nat lib mkn mnk kmn knm nkm nmk"
 LOGEXT=$CC.dat
 
-/bin/rm -f aos.$LOGEXT soa.$LOGEXT
+/bin/rm -f matmult_c.$LOGEXT
 
 for perm in $PERM
 do
