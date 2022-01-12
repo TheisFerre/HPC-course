e91c8fb8ad846cac3bdecbdc472979ff89cfc083
Andreas Aspe <s174197@student.dtu.dk>
2022-01-12 16:27:20 +0100

    Small changes to gauss_seidel.c

--- assignment2/gauss_seidel.c
+++ assignment2/gauss_seidel.c
@@ -6,20 +6,19 @@
 void
 gauss_seidel(int N, int iter_max, double tolerance, double ***u, double***f) {
 
-
     double fbnorm=1000000;
     double C= 1.0/6.0;
     double delta = 2.0/(N + 1.0);
     double delta2=delta*delta;
-    int k;
+    int k,z,y,x;
     double u_old;
     while(fbnorm>tolerance && k<iter_max)
     {fbnorm=0;
-        for(int z=1;z<N+1;z++)
+        for(z=1;z<N+1;z++)
         {
-            for(int y=1;y<N+1;y++)
+            for(y=1;y<N+1;y++)
             {
-                for(int x=1;x<N+1;x++)
+                for(x=1;x<N+1;x++)
                 {
                     u_old = u[z][y][x];
                     u[z][y][x]=C*(u[z-1][y][x] + u[z+1][y][x]+ \
