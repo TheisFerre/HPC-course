47b81b11b97664ec2cd65f3f5b71f022ca65a7e9
Jonathan <s174182@student.dtu.dk>
2022-01-13 11:28:02 +0100

    gauss_seidel parallel version

--- assignment2/gauss_seidel.c
+++ assignment2/gauss_seidel.c
@@ -15,10 +15,13 @@ gauss_seidel(int N, int iter_max, double tolerance, double ***u, double***f) {
     double u_old;
     while(fbnorm>tolerance && k<iter_max)
     {fbnorm=0;
+        #pragma omp for ordered(2) private(j,k)
         for(int z=1;z<N+1;z++)
         {
             for(int y=1;y<N+1;y++)
             {
+                #pragma omp ordered depend(sink: z-1,y)\
+                                    depend(sink: z,y-1)
                 for(int x=1;x<N+1;x++)
                 {
                     u_old = u[z][y][x];
@@ -27,7 +30,8 @@ gauss_seidel(int N, int iter_max, double tolerance, double ***u, double***f) {
                                   u[z][y][x-1] + u[z][y][x+1] + delta2*f[z][y][x]);
                     fbnorm+=(u[z][y][x]-u_old)*(u[z][y][x]-u_old);
                 }
-            }
+                #pragma omp ordered depend(source)
+            }   
         }
     fbnorm=sqrt(fbnorm);
     k++;
