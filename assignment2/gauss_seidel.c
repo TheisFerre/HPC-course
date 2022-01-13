573f2510cb6125384b0b8783ccdc1638160c4824
Jonathan <s174182@student.dtu.dk>
2022-01-13 11:55:17 +0100

    latest version with parallel inside while loop

--- assignment2/gauss_seidel.c
+++ assignment2/gauss_seidel.c
@@ -12,27 +12,31 @@ gauss_seidel(int N, int iter_max, double tolerance, double ***u, double***f) {
     double delta2=delta*delta;
     int k,z,y,x;
     double u_old;
-    while(fbnorm>tolerance && k<iter_max)
-    {fbnorm=0;
-        #pragma omp for ordered(2) private(x,y,z)
-        for(int z=1;z<N+1;z++)
-        {
-            for(y=1;y<N+1;y++)
-            {
-                #pragma omp ordered depend(sink: z-1,y)\
-                                    depend(sink: z,y-1)
-                for(int x=1;x<N+1;x++)
-                {
-                    u_old = u[z][y][x];
-                    u[z][y][x]=C*(u[z-1][y][x] + u[z+1][y][x]+ \
-                                  u[z][y-1][x] + u[z][y+1][x]+ \
-                                  u[z][y][x-1] + u[z][y][x+1] + delta2*f[z][y][x]);
-                    fbnorm+=(u[z][y][x]-u_old)*(u[z][y][x]-u_old);
+    
+        while(fbnorm>tolerance && k<iter_max)
+            #pragma omp parallel
+            {   
+                {fbnorm=0;
+                    #pragma omp for schedule(static,1) ordered(2) private(x,y,z)
+                    for(z=1;z<N+1;z++)
+                    {
+                        for(y=1;y<N+1;y++)
+                        {
+                            #pragma omp ordered depend(sink: z-1,y)\
+                                                depend(sink: z,y-1)
+                            for(x=1;x<N+1;x++)
+                            {
+                                u_old = u[z][y][x];
+                                u[z][y][x]=C*(u[z-1][y][x] + u[z+1][y][x]+ \
+                                            u[z][y-1][x] + u[z][y+1][x]+ \
+                                            u[z][y][x-1] + u[z][y][x+1] + delta2*f[z][y][x]);
+                                fbnorm+=(u[z][y][x]-u_old)*(u[z][y][x]-u_old);
+                            }
+                            #pragma omp ordered depend(source)
+                        }   
+                    }
+                fbnorm=sqrt(fbnorm);
+                k++;
                 }
-                #pragma omp ordered depend(source)
-            }   
         }
-    fbnorm=sqrt(fbnorm);
-    k++;
-    }
 }
