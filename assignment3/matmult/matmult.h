d8badc30b0883219caad8ec7519db3baf36ffc1d
TheisFerre <theisferre@gmail.com>
2022-01-19 09:26:43 +0100

    added week1 matmult

---
+++ assignment3/matmult/matmult.h
@@ -0,0 +1,21 @@
+void matmult_nat(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_lib(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_mkn(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_mnk(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_kmn(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_knm(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_nkm(int M, int N, int K, double **A, double **B, double **C);
+
+void matmult_nmk(int M, int N, int K, double **A, double **B, double **C);
+
+<<<<<<< HEAD
+void matmult_blk(int M, int N, int K, double **A, double **B, double **C, int bs);
+=======
+void matmult_blk(int M, int N, int K, double **A, double **B, double **C);
+>>>>>>> fc5d479cd342ca4db92350cb97c4468dc21c80b8
