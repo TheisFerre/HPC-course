5ec9a5dc5f8238bae7a52fe21e712570a710a810
Benjamin Starostka Jakobsen <s184198@student.dtu.dk>
2022-01-20 11:56:53 +0100

    latest update

--- assignment3/matmult/gpulib.cu
+++ assignment3/matmult/gpulib.cu
@@ -4,6 +4,7 @@
 #include <helper_cuda.h>
 
 #define BLOCK_SIZE 16
+#define IDX2C(i,j,ld) (((j)*(ld))+(i) // https://docs.nvidia.com/cuda/cublas/index.html compatible way to index 2d matrices in C for cublas
 
 extern "C" {
    void matmult_gpulib(int M, int N, int K, double *A_h, double *B_h, double *C_h); 
@@ -40,12 +41,13 @@ void matmult_gpulib(int M, int N, int K, double *A_h, double *B_h, double *C_h)
     // B = K X N
     // C = M X N
 
+    // Column Major: Leading Dimension (ld) = # of rows 
     // first dimension of A (M X K)
-    int lda = K; // M
+    int lda = K;
     // first dimension of B (K X N)
-    int ldb = N; // K
+    int ldb = N;
     // first dimension of C (M X N)
-    int ldc = N; // M
+    int ldc = N;
 
     const double alf = 1.0;
     const double bet = 0;
