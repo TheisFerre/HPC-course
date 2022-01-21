da119a517c5620169581737a783bd77f7cf66d5f
TheisFerre <theisferre@gmail.com>
2022-01-21 13:16:17 +0100

    all data

--- assignment3/matmult/profile-batch.sh
+++ assignment3/matmult/profile-batch.sh
@@ -12,10 +12,6 @@ export TMPDIR=$__LSF_JOB_TMPDIR__
 export MFLOPS_MAX_IT=1
 
 SIZE=2048
-GPU_VERSION=5
+GPU_VERSION=gpulib
  
-nv-nsight-cu-cli -o profile_$GPU_VERSION \ 
-    --section MemoryWorkloadAnalysis \ 
-    --section MemoryWorkloadAnalysis_Chart \ 
-    --section ComputeWorkloadAnalysis \ 
-./matmult_f.nvcc $GPU_VERSION $SIZE $SIZE $SIZE
+nv-nsight-cu-cli -f -o profile_$GPU_VERSION --section MemoryWorkloadAnalysis --section MemoryWorkloadAnalysis_Chart --section ComputeWorkloadAnalysis --section SpeedOfLight ./matmult_f.nvcc $GPU_VERSION $SIZE $SIZE $SIZE
---
+++ assignment3/matmult/test-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#BSUB -J test3
+#BSUB -o test3_%J.out
+#BSUB -e test3_%J.err
+#BSUB -q hpcintrogpu
+#BSUB -R "rusage[mem=8096]"
+#BSUB -n 16 #IF USING ALL THREADS POSSIBLE
+#BSUB -W 60
+#BSUB -N
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "span[hosts=1]"
+
+# gpu5 compared to CPU DGEMM and the other gpu_# kernels
+
+MATRIX_SIZES="64 128 256 512 1024 2048 4096"
+
+export MATMULT_RESULT=0
+export MATMULT_COMPARE=0
+export MKL_NUM_THREADS=16
+export MFLOPS_MAX_IT=100
+
+for size in $MATRIX_SIZES
+do
+    rm -f results/test_all_$size.txt
+    ./matmult_f.nvcc gpu2 $size $size $size >> results/test_all_$size.txt
+    ./matmult_f.nvcc gpu3 $size $size $size >> results/test_all_$size.txt
+    ./matmult_f.nvcc gpu4 $size $size $size >> results/test_all_$size.txt
+    ./matmult_f.nvcc gpu5 $size $size $size >> results/test_all_$size.txt
+    ./matmult_f.nvcc gpulib $size $size $size >> results/test_all_$size.txt
+    ./matmult_f.nvcc lib $size $size $size >> results/test_all_$size.txt
+done
+
